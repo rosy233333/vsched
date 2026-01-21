bec8b52edd9a8901abf90300e618ba2912746e7f
rosy <lc_rosy@qq.com>
2026-01-21 12:19:30 +0800

    发布到crates.io的版本

--- config/build.rs
+++ config/build.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 fn main() {
     let out_dir = std::env::var("OUT_DIR").unwrap();
     let out_path = Path::new(&out_dir).join("mut_cfgs.rs");
-    let rq_cap: usize = option_env!("RQ_CAP").unwrap_or("1").parse().unwrap();
+    let rq_cap: usize = option_env!("RQ_CAP").unwrap_or("256").parse().unwrap();
     let smp: usize = option_env!("SMP").unwrap_or("1").parse().unwrap();
     assert!(rq_cap.is_power_of_two());
 
